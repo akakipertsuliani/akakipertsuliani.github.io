0b00c3447748668de50d212da0072923fbeb3254
akakipertsuliani <akaki@kopitnari.net>
2023-09-29 23:18:19 +0400

    upgradeProcces

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementsByClassName("contact-container")[0].addEventListener("click", () => {
+        window.open("https://github.com/akakipertsuliani", "target=_blank");
+    });
+
+    document.getElementsByClassName("contact-container")[2].addEventListener("click", () => {
+        window.open("https://facebook.com/akaki.ferculiani.12", "target=_blank");
+    });
+});
