f49ced17f9b92b94212fbd6fdd9113aee9b0d093
akakipertsuliani <akaki@kopitnari.net>
2023-10-02 00:10:14 +0400

    upgradeUpAndDown

--- script.js
+++ script.js
@@ -42,12 +42,6 @@ document.addEventListener("DOMContentLoaded", () => {
         location.href = "#skill";
     });
 
+    let textAnimate = KUTE.to('#wellcome-text', { text: "Hello, world" }, { duration: 4500 }).start(); 
 
-    document.getElementsByClassName("contact-container")[0].addEventListener("click", () => {
-        window.open("https://github.com/akakipertsuliani", "target=_blank");
-    });
-
-    document.getElementsByClassName("contact-container")[2].addEventListener("click", () => {
-        window.open("https://facebook.com/akaki.ferculiani.12", "target=_blank");
-    });
 });
