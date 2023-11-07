07e2ba4aeb98b4fe6c76b45acd40f40a817fd57b
akakipertsuliani <akaki@kopitnari.net>
2023-11-07 23:44:45 +0400

    newUpdate

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+window.addEventListener("load", () => {
+    const loadingPage = document.querySelector('.loader-page');
+    loadingPage.style.display = 'none';
+});
+
 document.addEventListener("DOMContentLoaded", () => {
     let navBarPosition = false;
     let previousScrollY = window.scrollY;
@@ -9,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
             document.getElementById("header").style.top = "-9vh";
         }
         previousScrollY = currentScrollY;
-    })
+    });
 
     document.getElementById("menu-bar").addEventListener("click", () => {
         let distance = navBarPosition ? "200%" : "100%";
@@ -18,39 +23,73 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("nav-container").addEventListener("click", () => {
             document.getElementById("nav-container").style.left = "200%";
             navBarPosition = !navBarPosition;
-        })
-    })
-
-    const carousel = document.querySelector(".skill-container");
-    let isDragging = false;
-    let startX, scrollLeft;
-
-    carousel.addEventListener("mousedown", (e) => {
-        isDragging = true;
-        startX = e.pageX - carousel.offsetLeft;
-        scrollLeft = carousel.scrollLeft;
+        });
     });
 
-    carousel.addEventListener("mouseleave", () => {
-        carousel.style = "cursor: grab;"
-        isDragging = false;
+    let position1 = 0;
+    let position2 = 0;
+    let position3 = 0;
+    let startY1 = 120;
+    let startY2 = 120;
+    let startY3 = 120;
+
+    document.querySelectorAll(".text-over-container").forEach((button, index) => {
+        button.addEventListener("click", () => {
+            switch (index) {
+                case 0:
+                    if (position1) {
+                        document.querySelectorAll(".img-c-1").forEach((element) => {
+                            startY1 = 120;
+                            element.style = `transform: translateY(${startY1}%); opacity: 0;`;
+                        })
+                        document.getElementsByClassName("fa-solid")[1].style.transform = "rotate(0)";
+                        position1 = 0;
+                        break;
+                    }
+                    document.querySelectorAll(".img-c-1").forEach((element) => {
+                        element.style = `transform: translateY(${startY1}%); opacity: 1;`;
+                        startY1 += 120;
+                    });
+                    document.getElementsByClassName("fa-solid")[1].style.transform = "rotate(180deg)";
+                    position1 = 1;
+                    break;
+                case 1:
+                    if (position2) {
+                        document.querySelectorAll(".img-c-2").forEach((element) => {
+                            startY2 = 120;
+                            element.style = `transform: translateY(${startY2}%); opacity: 0;`;
+                        })
+                        document.getElementsByClassName("fa-solid")[2].style.transform = "rotate(0)";
+                        position2 = 0;
+                        break;
+                    }
+                    document.querySelectorAll(".img-c-2").forEach((element) => {
+                        element.style = `transform: translateY(${startY2}%); opacity: 1;`;
+                        startY2 += 120;
+                    });
+                    document.getElementsByClassName("fa-solid")[2].style.transform = "rotate(180deg)";
+                    position2 = 1;
+                    break;
+                case 2:
+                    if (position3) {
+                        document.querySelectorAll(".img-c-3").forEach((element) => {
+                            startY3 = 120;
+                            element.style = `transform: translateY(${startY3}%); opacity: 0;`;
+                        });
+                        document.getElementsByClassName("fa-solid")[3].style.transform = "rotate(0)";
+                        position3 = 0;
+                        break;
+                    }
+                    document.querySelectorAll(".img-c-3").forEach((element) => {
+                        element.style = `transform: translateY(${startY3}%); opacity: 1;`;
+                        startY3 += 120;
+                    });
+                    document.getElementsByClassName("fa-solid")[3].style.transform = "rotate(180deg)";
+                    position3 = 1;
+                    break;
+            };
+        });
     });
-
-    carousel.addEventListener("mouseup", () => {
-        carousel.style = "cursor: grab;"
-        isDragging = false;
-    });
-
-    carousel.addEventListener("mousemove", (e) => {
-        if (!isDragging) return;
-        e.preventDefault();
-        carousel.style = "cursor: grabbing;"
-        const x = e.pageX - carousel.offsetLeft;
-        const walk = (x - startX) * 5; 
-        carousel.scrollLeft = scrollLeft - walk;
-    });
-
-
 });
 
 let textAnimate = KUTE.to('#wellcome-text', { text: "Hello, world" }, { duration: 2500 }).start();
