4866e34f20bdd3c1090a1e27a600c2fc1b65a11e
akakipertsuliani <akaki@kopitnari.net>
2023-12-16 00:36:23 +0400

    littleBitUpdate

--- contactScript.js
+++ contactScript.js
@@ -3,16 +3,21 @@ const form = document.forms['submit-to-google-sheet'];
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    document.getElementById("response").innerHTML = "Please wait...";
+    document.getElementById("submit-button").textContent = "Please wait...";
+    document.getElementById("submit-button").disabled = true;
 
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
         .then(response => {
-            document.getElementById("response").innerHTML = "Send Succesfuly🤩"
+            document.getElementById("submit-button").textContent = "Send Succesfuly🤩";
             setTimeout(() => {
                 document.getElementById("name-field").value = "";
                 document.getElementById("email-field").value = "";
                 document.getElementById("message-field").value = "";
-                document.getElementById("response").innerHTML = "";
+                document.getElementById("submit-button").textContent = "Send";
+                document.getElementById("submit-button").disabled = false;
             }, 3500);
-        }).catch(error => document.getElementById("response").innerHTML = "Please Try Again😘");
+        }).catch(error => {
+            document.getElementById("submit-button").textContent = "Please Try Again😘"
+            document.getElementById("submit-button").disabled = false;
+        });
 });
