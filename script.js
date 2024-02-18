5793989e630069d47f0b1c51c17d3dc93b5cd00c
akakipertsuliani <akaki@pertsuliani>
2024-02-18 11:38:52 +0400

    newUpdateCome

--- script.js
+++ script.js
@@ -22,6 +22,27 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         previousScrollY = currentScrollY;
     });
+
+    let navBar = document.querySelector(".nav-bar");
+    let menuButton = document.querySelector(".menu-bar");
+    let navigationEl = document.querySelectorAll(".element");
+
+    menuButton.addEventListener("click", () => {
+        navBar.classList.toggle("active");
+    })
+
+    navigationEl.forEach((element) => {
+        element.addEventListener("click", () => {
+            navBar.classList.remove("active");
+        })
+    })
+
+    document.addEventListener("click", (e) => {
+        if (!navBar.contains(e.target) && !menuButton.contains(e.target) && window.innerWidth < 1024) {
+            navBar.classList.remove("active");
+        }
+    })
+
 });
 
 let textAnimate = KUTE.to('.text1', { text: "Hello, world" }, { duration: 2500 }).start();
